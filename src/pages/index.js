3dec56c7788ade0553705e87b236f19315cede68
Mateusz Podlasin <podlas29@gmail.com>
2021-06-30 17:09:19 +0200

    feat: convert to elements template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import { API } from '@stoplight/elements';
+
+require('@stoplight/elements/styles.min.css');
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+      <API 
+        apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml"
+        router={typeof window === 'undefined' ? 'memory' : 'history'} 
+      />
+    </div>
 }
